7a0ae310342dcbcbab1d8b6775cd867c58db7fe9
Imad <63809553+MadDoesGithub@users.noreply.github.com>
2023-01-06 16:28:48 -0800

    embed

--- theme.config.tsx
+++ theme.config.tsx
@@ -21,6 +21,13 @@ const config: DocsThemeConfig = {
         titleTemplate: '%s – BerryByte Docs',
       }
     },
+    head: (
+      <>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta property="og:title" content="Nextra" />
+        <meta property="og:description" content="The next site builder" />
+      </>
+    ),
   }
   
 
