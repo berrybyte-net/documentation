0a80d23fb66e7405358618842e8235e71c654ef9
Imad <63809553+MadDoesGithub@users.noreply.github.com>
2023-01-07 18:15:33 -0800

    fix: changing up some hyperlinks

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,35 +1,35 @@
-import React from 'react';
-import { DocsThemeConfig } from 'nextra-theme-docs';
+import React from 'react'
+import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-    logo: <span className="font-bold text-2xl">Berry<span className="text-lime-400">Byte</span> Docs</span>,
-    project: {
-        link: 'https://github.com/berrybyte-net/docs',
-    },
-    chat: {
-        link: 'https://discord.gg/Wc9wMZxQ2D',
-    },
-    docsRepositoryBase: 'https://github.com/berrybyte-net/docs',
-    footer: {
-        text: '© 2022 BerryByte Limited and contributors. Built with lots of coffee and love 💖',
-    },
-    sidebar: {
-        defaultMenuCollapseLevel: 0
+  logo: <span className='font-bold text-2xl'>Berry<span className='text-lime-400'>Byte</span> Docs</span>,
+  project: {
+    link: 'https://github.com/berrybyte-net/documentation',
+  },
+  chat: {
+    link: 'https://discord.gg/Wc9wMZxQ2D',
+  },
+  docsRepositoryBase: 'https://github.com/berrybyte-net/documentation/tree/main/',
+  footer: {
+    text: '© 2022 BerryByte Limited. All rights reserved. Built with lots of coffee and love 💖',
+  },
+  sidebar: {
+      defaultMenuCollapseLevel: 0
     },
     useNextSeoProps() {
-        return {
-            titleTemplate: '%s – BerryByte Docs',
-        };
+      return {
+        titleTemplate: '%s – BerryByte Docs',
+      }
     },
     head: (
-        <>
-            <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-            <meta property="og:title" content="BerryByte Docs"/>
-            <meta property="og:description" content="Public community-written documentation for all game/virtual server related topics."/>
-        </>
-    ),
-    primaryHue: 332,
-};
+    <>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta property="og:title" content="BerryByte Docs" />
+      <meta property="og:description" content="A public community-written documentation for all game-server/virtual server related topics." />
+    </>
+  ),
+  primaryHue: 332,
+  }
+  
 
-
-export default config;
+export default config
